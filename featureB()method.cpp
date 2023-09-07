a29c8c0564e733c0657360e459053dd1773edf85
Zainahowaidah <g00089315@aus.edu>
2023-09-07 10:20:16 +0400

    Feature B added

---
+++ featureB()method.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void printDone{} {
+    cout << "done" << endl;
+
+}
+
+
+int main()
+{
+    cout << "Hello World";
+    printDone();
+    return 0;
+}
